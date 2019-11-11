fa974b8548e3ac0e7b4c463e4f244355cdfd9218
mshanemc <shane.mclaughlin@salesforce.com>
2019-11-11 12:10:49 -0600

    script updates

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,5 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
-sfdx force:package:install --pacakge 04t41000002Q3o0AAC -w 20
+sfdx force:package:install --package 04t41000002Q3o0AAC -w 20
 sfdx force:source:push
 sfdx automig:load -d data
 sfdx force:user:password:generate
