2d79d8ab2cc2131a3a3f1f0a625210cb3ac2598b
mshanemc <shane.mclaughlin@salesforce.com>
2019-11-11 12:39:36 -0600

    with permset and assign

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,7 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
 sfdx force:package:install --package 04t41000002Q3o0AAC -w 20
 sfdx force:source:push
+sfdx force:user:permset:assign -n workshop
 sfdx automig:load -d data
 sfdx force:user:password:generate
 sfdx force:org:open
