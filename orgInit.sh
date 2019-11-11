7d98580fd0aa150751ef9fcb8a720cd07e491a38
mshanemc <shane.mclaughlin@salesforce.com>
2019-11-11 12:04:09 -0600

    pulls and data load from source org

--- orgInit.sh
+++ orgInit.sh
@@ -1,3 +1,6 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
+sfdx force:package:install --pacakge 04t41000002Q3o0AAC -w 20
 sfdx force:source:push
+sfdx automig:load -d data
+sfdx force:user:password:generate
 sfdx force:org:open
